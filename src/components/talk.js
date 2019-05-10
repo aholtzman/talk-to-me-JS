536b8e1fd3928395757df856c9024e69af5c03c4
Adam Holtzman <adam.holtzman@gmail.com>
2019-05-10 15:53:26 -0400

    spelling error fix

--- src/components/talk.js
+++ src/components/talk.js
@@ -14,7 +14,7 @@ function speak (message, v = 7) {
 
 export default class Talk extends Component {
 
-  handelClick() {
+  handleClick() {
     let message = text[getRandomInt(text.length)]
     let voice = getRandomInt(60)
     speak(message, voice)
@@ -25,7 +25,7 @@ export default class Talk extends Component {
   render() {
     return(
       <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundImage: 'url("https://www.thehappycatsite.com/wp-content/uploads/2017/11/kitten-tabby.jpg")', minHeight: '90vh', backgroundSize: 'cover', backgroundPosition: 'center'}}>
-        <button style={{padding: '1.5rem', backgroundColor: '#191966', color: '#fff', fontSize: '18px', top: 'calc(100vh - 200px)', border: '0', position: 'fixed', borderRadius: '50px', textTransform: 'uppercase'}} onClick={this.handelClick}>tell me something nice</button>
+        <button style={{padding: '1.5rem', backgroundColor: '#191966', color: '#fff', fontSize: '18px', top: 'calc(100vh - 200px)', border: '0', position: 'fixed', borderRadius: '50px', textTransform: 'uppercase'}} onClick={this.handleClick}>tell me something nice</button>
         <p style={{top: 'calc(100vh - 260px)', position: 'fixed'}}>(press the button, make sure your sound is on.)</p>
       </div>
     )
